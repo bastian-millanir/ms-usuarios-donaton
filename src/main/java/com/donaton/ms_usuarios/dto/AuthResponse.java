e6cc2f545a0ef1274c23b84e1af092287e2b7cab
bastian-millanir <bastian.millanir@gmail.com>
2026-05-21 22:19:26 -0400

    feat: auth response

---
+++ src/main/java/com/donaton/ms_usuarios/dto/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.donaton.ms_usuarios.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthResponse {
+    private String token;
+    private String username;
+    private String email;
+    private String mensaje;
+}
